aef548da460452ae98b48d78beec414411e3a772
Vitor Santos <santoss.coding@gmail.com>
2024-12-04 14:49:07 -0300

    chore: setup Vite project with React and NextUI
    - Initialized Vite project with React template.
    - Installed NextUI and its dependencies.
    - Configured Vite for NextUI compatibility.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@nextui-org/button"
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Word!</h1>
+      <Button color="primary">Click me</Button>
+    </>
+  )
+}
+
+export default App
